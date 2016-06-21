6a934610e27be0a7f6af476ad4c86096d24c3ef3
githmith <vuminhthanh2305@gmail.com>
2016-06-21 05:26:26 -0400

    Make life harder for no reason

--- BirthdayWish.java
+++ BirthdayWish.java
@@ -3,46 +3,56 @@ import java.io.IOException;
 public class BirthdayWish{
     
     static int level = 6;
+    static boolean isActive = false;
+    static boolean missingDecodeMethod = true;
     
     public static void main(String []args){
-        try{
-            Process p = Runtime.getRuntime().exec( "say Happy belated Birthday" );
-        } catch(IOException e){
-            System.out.println("Can't execute command line!");
+        if(isActive){
+            try{
+                Process p = Runtime.getRuntime().exec( "say Happy belated Birthday" );
+            } catch(IOException e){
+                System.out.println("Can't execute command line!");
+            }
+            
+            System.out.println(b_message());
+        }else{
+            System.out.println("[WARNING: You may need to activate the class and implement some missing code!]");
         }
-        
-        System.out.println(b_message());
     }
     
     public static String b_message (){
         String message = "";
         
-        // insert opening
-        message += "\n\n#========================#\n# ";
-        message += "Happy belated Birthday #\n# ";
-        message += "     to Huyyyy !!!     #\n# ";
-        message += "                       #\n# ";
+        if(!missingDecodeMethod){
+            // insert opening
+            message += "\n\n#========================#\n# ";
+            message += "Happy belated Birthday #\n# ";
+            message += "     to Huyyyy !!!     #\n# ";
+            message += "                       #\n# ";
 
-        
-        // insert wishes
-        String partA = "In{iy uutm ";
-        String partB = "z{uuox";
-        String wish1 = "sgtnp qnukx";
-        String wish2 = "sg sg}ty";
-        String wish3 = "zngtnl iuutm";
-        int b_age = 10100;
-        
-        String wishes = " " + decode(partA) + btd(b_age) + " " + decode(partB) + "  #\n# ";
-        wishes += "                       #\n# ";
-        wishes += "#---  " + decode(wish1) + " ---# #\n# ";
-        wishes += "#---   " + decode(wish2) + "  ---# #\n# ";
-        wishes += "#--- " + decode(wish3) + " ---# #\n#";
-        wishes += "                        #\n";
-        
-        message += wishes;
-        
-        // insert ending
-        message += "#========================#\n\n";
+            
+            // insert wishes
+            String partA = "In{iy uutm ";
+            String partB = "z{uuox";
+            String wish1 = "sgtnp qnukx";
+            String wish2 = "sg sg}ty";
+            String wish3 = "zngtnl iuutm";
+            int b_age = 10100;
+            
+            String wishes = " " + decode(partA) + btd(b_age) + " " + decode(partB) + "  #\n# ";
+            wishes += "                       #\n# ";
+            wishes += "#---  " + decode(wish1) + " ---# #\n# ";
+            wishes += "#---   " + decode(wish2) + "  ---# #\n# ";
+            wishes += "#--- " + decode(wish3) + " ---# #\n#";
+            wishes += "                        #\n";
+            
+            message += wishes;
+            
+            // insert ending
+            message += "#========================#\n\n";
+        }else{
+            System.out.println("[WARNING: decode method not found!]");
+        }
         
         return message;
     }
@@ -65,16 +75,17 @@ public class BirthdayWish{
         return output;
     }
     
+    //////////////////////////////////////////////////////
+    /// this method needs to be implemented
+    /// then set missingDecodeMethod variable to false
+    //////////////////////////////////////////////////////
     public static String decode(String input){
-        char[] arr = input.toCharArray();
-        String output = "";
-        for(int i = 0; i < arr.length; i++){
-            char x = arr[i];
-            if(x != ' '){
-                x = (char)((int)arr[i] - level);
-            }
-            output += x;
-        }
-        return output;
+//        missingDecodeMethod = false;
+        return "{the-code-for-this-is-missing}";
+    }
+    
+    public static boolean activate(){
+        isActive = true;
+        return isActive;
     }
 }
